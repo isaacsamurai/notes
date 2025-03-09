f7c08ece1138fb869677d3eee8ab74894f76b7df
Isaac Samuel <isaacsamuel002@gmail.com>
2025-03-08 21:43:25 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hello Word";
+});
+
+Route::get('/about', function(){
+    echo 'About us';
 });
