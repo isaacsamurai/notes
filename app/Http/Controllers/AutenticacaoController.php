45463d91fb94ab9a72a795965a7492596926477b
Isaac Samuel <isaacsamuel002@gmail.com>
2025-03-20 01:22:54 -0300

    criando mensagens de erros de validação

--- app/Http/Controllers/AutenticacaoController.php
+++ app/Http/Controllers/AutenticacaoController.php
@@ -9,13 +9,31 @@ class AutenticacaoController extends Controller
     public function login() {
         return view('login');
     }
-    
+
     public function loginSubmit(Request $request){
 
-        echo $request->input('text_username');
-        echo '<br>';
-        echo $request->input('text_username');
+        $request->validate
+        (
+            //form validation
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            //error messages
+            [
+                'text_username.required' => 'O username é obrigatório',
+                'text_username.email' => 'O username precisa ser um email válido',
+                'text_password.required' => 'A senha é orbigatória',
+                'text_password.min' => 'A senha precisa ter no mínimo 6 caracteres',
+                'text_password.max' =>'A senha deve ter no máximo 16 caracteres'
+            ]
 
+        );
+
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo "tamo aqui";
     }
 
     public function logout(){
